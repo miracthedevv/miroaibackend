919f44c2605937df24d06b094dcae030b2e4ad9a
MİRAÇ TAHİRCAN YILMAZ <muzik.kargasasi18@gmail.com>
2026-03-26 20:31:52 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
 
     if (req.method === 'OPTIONS') return res.status(200).end();
-    if (req.method !== 'POST') return res.status(405).json({ error: 'POST lazım kanka!' });
+    if (req.method !== 'POST') return res.status(405).json({ error: 'Metod geçersiz.' });
 
     const NVIDIA_API_KEY = process.env.NVIDIA_APIKEY;    
     const TAVILY_API_KEY = process.env.TVLY_APIKEY;
